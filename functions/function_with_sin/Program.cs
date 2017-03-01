4f277a8921e271b9c83c4e8e1a1b89e8ccff0d63
Marisha101297 <marisha1012@gmail.com>
2017-03-01 12:16:34 +0500

    programm  page74number5

--- functions/function_with_sin/Program.cs
+++ functions/function_with_sin/Program.cs
@@ -8,8 +8,30 @@ namespace function_with_sin
 {
     class Program
     {
+        static double Vichislenie(double Chislitel, double Znamenatel)
+        {
+            double result = (Chislitel + Math.Sin(Chislitel)) / (Math.Sin(Znamenatel) + Znamenatel);
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            double peremennayaVchislitele = 2, peremennayaVznamenatele = 5;
+            double resultOfMethod = Vichislenie(peremennayaVchislitele, peremennayaVznamenatele);
+
+            double y = 0;
+
+            y = y + resultOfMethod;
+            peremennayaVchislitele = 6;
+            peremennayaVznamenatele = 3;
+            resultOfMethod = Vichislenie(peremennayaVchislitele, peremennayaVznamenatele);
+            y = y + resultOfMethod;
+            peremennayaVchislitele = 1;
+            peremennayaVznamenatele = 4;
+            resultOfMethod = Vichislenie(peremennayaVchislitele, peremennayaVznamenatele);
+            y = y + resultOfMethod;
+            Console.WriteLine(y);
+
         }
     }
 }
