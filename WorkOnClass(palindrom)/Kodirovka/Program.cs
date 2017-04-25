d8f1a9db5e2e139e77878bcac27afd205dee6d52
Marishka <Marishka@MARISHKA-PC>
2017-04-25 22:45:37 +0500

    palindrom(search)

---
+++ WorkOnClass(palindrom)/Kodirovka/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodirovka
+{
+    class Program
+    {
+        static char[,] Code(string text)
+        {
+            Random get = new Random();
+
+            char[] array = new char[text.Length];
+            int[,] mass = new int[text.Length, 2];
+            char[,] trrt = new char[text.Length, 2];
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                array[i] = Convert.ToChar(text[i]);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                mass[i, 0] = (int)array[i];
+                mass[i, 1] = get.Next(10);
+                mass[i, 0] = mass[i, 0] + mass[i, 1];
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                trrt[i, 0] = Convert.ToChar(mass[i, 0]);
+                trrt[i, 1] = Convert.ToChar(mass[i, 1]);
+            }
+
+            return trrt;
+
+        }
+
+
+        static char[] Decode(char[,] Code)
+        {
+            int[] erre = new int[Code.Length / 2];
+            char[] trrt = new char[Code.Length / 2];
+
+            for (int i = 0; i < Code.Length / 2; i++)
+            {
+                // erre[i,0] = (int)Kod[i,0];
+                erre[i] = (int)Code[i, 0] - (int)Code[i, 1];
+            }
+
+            for (int i = 0; i < Code.Length / 2; i++)
+            {
+                trrt[i] = Convert.ToChar(erre[i]);
+            }
+
+            return trrt;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Marishka\Desktop\Евтушенко.txt");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(Code(text)[i, 0]);
+            }
+
+            Console.Write("\r\n");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(Decode(Code(text))[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WorkOnClass(palindrom)/WorkOnClass/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOnClass
+{
+    class Program
+    {
+
+        static bool Palindrom(string slovo)
+        {
+
+
+            int s = 0;
+            char[] arra = new char[slovo.Length];
+
+            for (int i = 0; i < slovo.Length; i++)
+            {
+                arra[i] = slovo[i];
+            }
+
+            for (int i = 0; i < slovo.Length / 2; i++)
+            {
+                if (arra[i] == arra[arra.Length - (1 + i)])
+                {
+                    s++;
+                }
+                else
+                {
+                    //Console.WriteLine("НЕ Палиндром");
+                    break;
+                }
+            }
+            if (s == slovo.Length / 2 & (slovo != ""))
+                Console.WriteLine("Палиндром  {0}", slovo);
+
+            
+
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+            string str = System.IO.File.ReadAllText(@"C:\Users\Marishka\Desktop\Евтушенко.txt");
+
+            string[] arr = str.Split(new Char[] { ' ', '-', '"', ',', '.', ':', '\r', '\t', '!', '?', ';', '\n', '\0', });
+            // string[] arr = str.Split(new char[] { ' ', ';', '-', '_', '@', '>', '<', '.' });
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Palindrom(arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
